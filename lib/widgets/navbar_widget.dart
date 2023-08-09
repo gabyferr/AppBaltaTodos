46146262840ab360710b0d28b85c6fdee7b55e0a
gabyferr <70179462+gabyferr@users.noreply.github.com>
2023-08-09 08:01:39 -0300

    refatoração do navbar.

--- lib/widgets/navbar_widget.dart
+++ lib/widgets/navbar_widget.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:provider/provider.dart';
+import 'package:teste_gaby/controllers/todo_controller.dart';
+import 'package:teste_gaby/store/app_store.dart';
 
 class NavBar extends StatelessWidget {
-  const NavBar({super.key});
+  NavBar({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final store = Provider.of<AppStore>(context);
+    final controller = TodoController(store);
     return Container(
       width: double.infinity,
       height: 80,
@@ -12,33 +18,48 @@ class NavBar extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          TextButton(
-            onPressed: () {},
-            child: const Text(
-              "Hoje",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+          Observer(
+            builder: (BuildContext context) => TextButton(
+              onPressed: () {
+                controller.changeSelection("today");
+              },
+              child: Text(
+                "Hoje",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: store.currentState == "today"
+                        ? FontWeight.bold
+                        : FontWeight.w100),
               ),
             ),
           ),
-          TextButton(
-            onPressed: () {},
-            child: const Text(
-              "Amanhã",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.w100,
+          Observer(
+            builder: (BuildContext context) => TextButton(
+              onPressed: () {
+                controller.changeSelection("tomorrow");
+              },
+              child: Text(
+                "Amanhã",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: store.currentState == "tomorrow"
+                        ? FontWeight.bold
+                        : FontWeight.w100),
               ),
             ),
           ),
-          TextButton(
-            onPressed: () {},
-            child: const Text(
-              "Todas",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.w100,
+          Observer(
+            builder: (BuildContext context) => TextButton(
+              onPressed: () {
+                controller.changeSelection("all");
+              },
+              child: Text(
+                "Todas",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: store.currentState == "all"
+                        ? FontWeight.bold
+                        : FontWeight.w100),
               ),
             ),
           ),
